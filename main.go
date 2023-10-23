a1a8c976de8b34436ebb22d65c640e86cbb6759e
robsonandradev <robsom.s.a@gmail.com>
2023-10-23 15:06:32 -0300

    Add gorilla/mux as web server

--- main.go
+++ main.go
@@ -1,2 +1,36 @@
 package main
 
+import (
+  "net/http"
+  "log"
+  "fmt"
+  "github.com/gorilla/mux"
+  hc "github.com/robsonandradev/notes_api/use_cases/health"
+)
+
+func main() {
+  port := 3000
+  router := mux.NewRouter()
+  hc.Set(router)
+  router.Use(loggingMiddleware, headerMiddleware)
+  svc := &http.Server{
+    Handler: router,
+    Addr: fmt.Sprintf("0.0.0.0:%d", port),
+  }
+  log.Println(fmt.Sprintf("magic happens on %d", port))
+  log.Fatal(svc.ListenAndServe())
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    log.Println(r.RequestURI)
+    next.ServeHTTP(w, r)
+  })
+}
+
+func headerMiddleware(next http.Handler) http.Handler {
+  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    next.ServeHTTP(w, r)
+  })
+}
