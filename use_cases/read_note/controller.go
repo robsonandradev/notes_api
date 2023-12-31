f09e09f7201cec936cb9bb0ec3f668dbd67df9c0
robsonandradev <robsom.s.a@gmail.com>
2023-12-31 20:00:00 -0300

    Handle error in read note controller

--- use_cases/read_note/controller.go
+++ use_cases/read_note/controller.go
@@ -1,6 +1,7 @@
 package readnote
 
 import (
+  "log"
   "encoding/json"
   "net/http"
   "github.com/gorilla/mux"
@@ -9,18 +10,34 @@ import (
 
 type ReadNoteController struct {}
 
+func NewReadNoteController() *ReadNoteController {
+  return &ReadNoteController{}
+}
+
 func (c *ReadNoteController) Set(router *mux.Router) {
   router.HandleFunc("/notes", c.exec).Methods("GET")
 }
 
 func (c *ReadNoteController) exec(w http.ResponseWriter, r *http.Request) {
   repo, err := repos.NewNoteRepository("postgres")
-  if err != nil { panic(err) }
+  if err != nil {
+    internalServerError(w, err)
+    return
+  }
   readNoteSvc := NewReadNoteService(repo)
   author := r.URL.Query().Get("author")
   title  := r.URL.Query().Get("title")
   notes, err := readNoteSvc.GetNoteByAuthorAndTitle(author, title)
-  if err != nil { panic(err) }
+  if err != nil {
+    internalServerError(w, err)
+    return
+  }
   w.WriteHeader(http.StatusOK)
   json.NewEncoder(w).Encode(notes)
+}
+
+func internalServerError(w http.ResponseWriter, err error) {
+  log.Println(err)
+  w.WriteHeader(http.StatusInternalServerError)
+  json.NewEncoder(w).Encode(map[string]string{"error": "internal server error"})
 }
