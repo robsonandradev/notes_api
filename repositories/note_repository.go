eb3093c946694b3d918f37cdbd3ed0478905f57d
robsonandradev <robsom.s.a@gmail.com>
2023-12-31 18:03:02 -0300

    Add get notes route

--- repositories/note_repository.go
+++ repositories/note_repository.go
@@ -12,8 +12,33 @@ type INoteRepository interface {
   GetNoteByAuthorAndTitle(author, title string) (n []e.Note, err error)
 }
 
-type NoteRepository struct {}
+type NoteRepository struct {
+  PG DBConnection
+}
+
+func NewNoteRepository(connector string) (*NoteRepository, error) {
+  if connector == "postgres" {
+    ur := &NoteRepository{ PG: &PostgresCon{} }
+    return ur, nil
+  }
+  return &NoteRepository{}, fmt.Errorf("Unknow database connector")
+}
 
 func (nr *NoteRepository) CreateNote(author, title, content string) (n e.Note, err error) {
   return e.Note{}, fmt.Errorf("Something goes wrong!")
 }
+
+func (nr *NoteRepository) GetNoteByTitle(title string) (n []e.Note, err error) { return }
+
+func (nr *NoteRepository) GetNotesByAuthor(author string) (n []e.Note, err error) {
+  db, err := nr.PG.Connect()
+  if err != nil {
+    return []e.Note{}, err
+  }
+  defer nr.PG.Close(db)
+  notes := []e.Note{}
+  r := db.Where("author = ?", author).Find(&notes)
+  return notes, r.Error
+}
+
+func (nr *NoteRepository) GetNoteByAuthorAndTitle(author, title string) (n []e.Note, err error) { return }
--- use_cases/read_note/controller.go
+++ use_cases/read_note/controller.go
@@ -1,1 +1,26 @@
 package readnote
+
+import (
+  "encoding/json"
+  "net/http"
+  "github.com/gorilla/mux"
+  repos "github.com/robsonandradev/notes_api/repositories"
+)
+
+type ReadNoteController struct {}
+
+func (c *ReadNoteController) Set(router *mux.Router) {
+  router.HandleFunc("/notes", c.exec).Methods("GET")
+}
+
+func (c *ReadNoteController) exec(w http.ResponseWriter, r *http.Request) {
+  repo, err := repos.NewNoteRepository("postgres")
+  if err != nil { panic(err) }
+  readNoteSvc := NewReadNoteService(repo)
+  author := r.URL.Query().Get("author")
+  title  := r.URL.Query().Get("title")
+  notes, err := readNoteSvc.GetNoteByAuthorAndTitle(author, title)
+  if err != nil { panic(err) }
+  w.WriteHeader(http.StatusOK)
+  json.NewEncoder(w).Encode(notes)
+}
